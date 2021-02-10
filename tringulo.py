7600769f2d5bcd56de931f8e0d5f74c4ff38ce53
Carlos Alonso Martín <sr.titere@gmail.com>
2021-02-10 01:07:36 +0100

    falta refactorizar

--- tringulo.py
+++ tringulo.py
@@ -1,4 +1,6 @@
 import math
+import sys
+import getopt
 from pprint import pp
 
 def triangulo(num: int, valores):
@@ -6,7 +8,6 @@ def triangulo(num: int, valores):
     num_elemntos = (num*(num + 1))/2
     num_combinaciones = math.factorial(num_elemntos)
     matriz_trin = []
-    print("el numero de elementos es: ", num_elemntos)
     matriz_trin.append(valores)
     for i in range(num-1):
         matriz_trin.append([])
@@ -14,36 +15,99 @@ def triangulo(num: int, valores):
             n = abs(matriz_trin[i][j]-matriz_trin[i][j+1])
             for w in matriz_trin:
                 if n in w:
-                    return "combinacion no valida"
+                    raise Exception("combinacion no valida")
             # print(matriz_trin)
             matriz_trin[i+1].append(n)
     return matriz_trin
 
-def combi(num: int):
-    matriz = []
-    num_elementos = int((num*(num + 1))/2)
-    j=0
-    tam=0
-    for i in range(num_elementos):
-        for a in range(num_elementos):
-            matriz.append([])
-            matriz[tam].append(i)
-            n=a
-            c=0
-            while c < num:
-                if n not in matriz[tam]:
-                    matriz[tam].append(n)
-                    c +=1
-                n +=1
-            tam +=1
-    return matriz
 
-def main():
-    #elementos = [6,1,4]
-    pp(combi(3))
+def obtener_variaciones_sin_repeticion(set, k, result):
 
-    #print(triangulo(len(elementos), elementos))
+    n = len(set)
+    obtener_variaciones_sin_repeticion_rec(set, [], n, k, result)
 
 
+def obtener_variaciones_sin_repeticion_rec(set, prefix, n, k, result):
+
+    # En caso de que tengamos una variacion (subconjunto) validos
+    # de k elementos distintos.
+    if (k == 0):
+        result.append(prefix)
+        return
+
+    # One by one add all characters
+    # from set and recursively
+    # call for k equals to k-1
+    for i in range(n):
+
+        # Comprobamos si no existe el elemento en el subconjunto
+        # en caso que exista saltamos a la siguiente iteración
+        if set[i] in prefix:
+            continue
+        # Añadimos el elemento al subconjunto
+        lista = [set[i]]
+        newPrefix = prefix + lista
+
+        # De decrementamos k en la llamada recursiva ya que
+        # tenemos un nuevo elemento añadido al subconjunto
+        obtener_variaciones_sin_repeticion_rec(set, newPrefix, n, k - 1,result)
+
+
+
+
+def resultado(matriz):
+# imprimimos los resultados.
+    for lista in matriz:
+        #pp(lista)
+        try:
+            resultado = triangulo(len(lista),lista)
+            pp(resultado)
+        except Exception:
+            pass
+
+
+
+def main(argv):
+    try:
+        if len(argv) < 2:
+            num = int(input("Introduzca el número n de T sub n"
+                            +"\n(el numero de bolas por lado): "))
+        else:
+
+            num = int(argv[1])
+    except Exception:
+        print("Error introduzca un numero como parametro")
+        return
+
+    # ecuación numero triangular para sacar el numero de elementos (bolas)
+    # que tendra nuestro triangulo a partir del numero de bolas por lado.
+    # ref: https://es.wikipedia.org/wiki/N%C3%BAmero_triangular
+    num_elemntos = int((num*(num + 1))/2)
+
+    num_variaciones = int(math.factorial(num_elemntos)/math.factorial(num_elemntos - num))
+    if num > 5:
+        print(f"CUIDADO: el número introducido {num} "
+              + f"genera {num_variaciones} variaciones (subconjuntos) posibles.\n"
+              + "Todo numero introducido mayor de 5 va a tardar"
+              + " un buen rato, en función del valor y tu máquina, más vale que"
+              + " tengas una buena máquina")
+        if input("¿Deseas continuar? (y/n): ") != "y":
+            print("adios")
+            return
+    print("Calculando.....(Ctrl+c para matar el proceso)")
+
+
+    # inicializamos nuestra matriz que contendra los resultados
+    result= []
+
+    # obtenemos una lista de los todos los números de elementos (bolas)
+    lista_elementos = list(range(1,num_elemntos+1))
+
+    obtener_variaciones_sin_repeticion(lista_elementos, num, result)
+
+    pp("=============Resultados Validos========")
+    resultado(result)
+
+# punto de entrada
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
